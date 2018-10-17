8888f1328405afa953554fb06a440322cded0591
Alexandros w.w.w <harvesterstart@outlook.com>
2018-10-17 00:21:14 -0700

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[]=[
+    { id:11, name: 'Mr. Nic' },
+    { id:12, name: 'Mr. Nic' },
+    { id:13, name: 'Mr. Nic' },
+    { id:14, name: 'Mr. Nic' },
+    { id:15, name: 'Mr. Nic' },
+    { id:16, name: 'Mr. Nic' },
+    { id:17, name: 'Mr. Nic' },
+    { id:18, name: 'Mr. Nic' },
+    { id:19, name: 'Mr. Nic' },
+    { id:20, name: 'Mr. Nic' }
+]
